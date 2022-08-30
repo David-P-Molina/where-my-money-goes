c1c668ad8641026df8854cefe4a651943c9ab21d
David Moina <davemolina777@gmail.com>
2022-08-30 15:54:16 -0500

    Add Bulma CSS Framework

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :purchases
   resources :categories
   resources :budgets
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "budgets#index" 
 end
