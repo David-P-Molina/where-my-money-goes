bbfc20d4c153188dd858096b6703d691313ad37f
David Moina <davemolina777@gmail.com>
2022-08-31 10:14:27 -0500

    Create model relations

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,2 +1,3 @@
 class Budget < ApplicationRecord
+    has_many :categories, class_name: "category", foreign_key: "reference_id"
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   belongs_to :budget
+  has_many :purchases, class_name: "purchase", foreign_key: "reference_id"
 end
