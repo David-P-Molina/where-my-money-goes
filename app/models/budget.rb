59192c12893871a2fd7bc85636c7841b401dcfc7
David Moina <davemolina777@gmail.com>
2022-08-31 11:11:41 -0500

    Add budget purchase relationship, format application.html.erb

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,3 +1,4 @@
 class Budget < ApplicationRecord
     has_many :categories, class_name: "category", foreign_key: "reference_id"
+    has_many :purchases, class_name: "purchase", foreign_key: "reference_id"
 end
