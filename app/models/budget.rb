e958a7603d0e11dffa99e37006399df8bef49d59
David Moina <davemolina777@gmail.com>
2022-08-31 11:36:19 -0500

    Create schema, Create basic validations

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,11 @@
 class Budget < ApplicationRecord
     has_many :categories, class_name: "category", foreign_key: "reference_id"
     has_many :purchases, class_name: "purchase", foreign_key: "reference_id"
+
+    validates :name, :description, :starting_amount, :start, :end , presence: true
+    validates :starting_amount , numericality: true
+    validates :ending_amount, absence: true
+    validates :name, uniqueness: true
+
+    #Custom Validations
 end
